45fd02eca3f168c602a08a99e9be852d3e904f58
Matheus Costa <matheus.cvb@hotmail.com>
2024-09-09 19:20:34 -0300

    first commit

---
+++ array.js
@@ -0,0 +1,17 @@
+
+var numeros = [3, 5]; //array numerico
+
+var frutas = ["Laranja", "Goiaba", "Uva"]; //array numerico
+
+//Objeto array associativo
+var fruta = {
+    "nome": "Laranja",
+    "valor": 20.00
+}
+
+ //Array com objetos
+ var frutas = [
+     {"nome": "Laranja", "Valor":20.00},
+     {"nome": "Goiaba", "Valor": 20.00},
+     {"nome": "Uva", "Valor": 20.00},
+];
---
+++ teste4.js
@@ -0,0 +1,10 @@
+var idade = 20;
+
+switch (true) {
+    case (idade < 13):
+    console.log('Criança');
+    break;
+case (idade >= 13 && idade < 18):
+    console.log(´)    
+
+}
